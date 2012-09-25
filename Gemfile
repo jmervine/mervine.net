5bf18d5af66c091bcf0674553eb55d192cea5c05
Joshua Mervine <joshua@mervine.net>
2012-09-24 23:17:06 -0700

    added handling for autogenerating meta keywords and descriptions

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'nesta', '0.9.13'
 
 gem 'nesta-plugin-google-ads'
 gem 'nesta-plugin-sharethis'
+gem 'nesta-plugin-diskcached'
 
 #gem 'coderay'
 gem "ultraviolet", :require => "uv"
--- themes/rubyops/app.rb
+++ themes/rubyops/app.rb
@@ -36,6 +36,31 @@ module Nesta
         redirect "/"
       end
     end
+  end
+  class FileModel
+    def description
+      metadata('description') || metadata('summary') 
+    end
+    def keywords
+      metadata('keywords') || generated_keywords
+    end
 
+    private
+    def generated_keywords
+      keywords = ""
+      keywords << keywords_from_pages(categories)
+      keywords << keywords_from_pages(articles)
+    end
+
+    def keywords_from_pages(pages)
+      unless pages.empty?
+        return pages.map do |page|
+                 page.filename.split("/").last.split(".").first
+               end.map do |page|
+                 page.split("-").join(" ")
+               end.join(", ")
+      end
+      return ""
+    end
   end
 end
