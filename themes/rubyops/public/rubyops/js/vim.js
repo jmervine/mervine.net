3d18018de27d9941ebbc36ce6c7879c5224acace
Josh Mervine <jmervine@yp.com>
2013-08-10 11:06:11 -0700

    updating vim.js

--- themes/rubyops/public/rubyops/js/vim.js
+++ themes/rubyops/public/rubyops/js/vim.js
@@ -1,38 +1,41 @@
-function go(val) { $("html, body").animate({ scrollTop: val }, 200); }
-function vimUp() { go($(window).scrollTop()-100); }
-function vimDown() { go($(window).scrollTop()+100); }
-function vimTop() { go(0); }
-function vimBot() { go($('html').height()); }
-function vimPageUp() { go($(window).scrollTop()-$(window).height());}
-function vimPageDown() {go($(window).scrollTop()+$(window).height());}
-function vimHalfUp() { go($(window).scrollTop()-($(window).height()/2));}
-function vimHalfDown() {go($(window).scrollTop()+($(window).height()/2));}
-function vimSearch(name) { setTimeout(function() {$(name).focus(); }, 250); }
+var searchElement = 'form#searchform input#searchbox';
 
-$(window).load( function () {
-    var gTimes = 0;
-    $(window).keypress(function(event) {
-        switch (event.keyCode) {
-            case 106: vimDown(); break;
-            case 107: vimUp(); break;
-            case 71: vimBot(); break;
-            case 70: vimPageDown(); break;
-            case 66: vimPageUp(); break;
-            case 85: vimHalfUp(); break;
-            case 68: vimHalfDown(); break;
-            case 47: vimSearch('form#searchform input#searchbox'); break;
-            case 103:
-                if (gTimes === 1) {
-                    gTimes = 0;
-                    vimTop();
-                } else {
-                    gTimes = 1;
-                    setTimeout(function () { gTimes = 0; }, 1000);
-                }
-                break;
-            default:
-                console.log(event.keyCode);
+function go(val) {
+    if(ready){
+        ready=false;
+        $("html, body").animate({ scrollTop: val}, 200);
+        setTimeout(function(){ ready=true; },250);
+    }
+}
+
+function search() {
+    if (typeof searchElement!=='undefined') {
+        setTimeout(function() { $(searchElement).focus(); }, 250);
+    }
+}
+var ready=true,
+    g=0,
+    vim = {
+        106: function(){ go($(window).scrollTop()+100); },
+        107: function(){ go($(window).scrollTop()-100); },
+         71: function(){ go($('html').height()); },
+         70: function(){ go($(window).scrollTop()+$(window).height());},
+         66: function(){ go($(window).scrollTop()-$(window).height());},
+         85: function(){ go($(window).scrollTop()-($(window).height()/2));},
+         68: function(){ go($(window).scrollTop()+($(window).height()/2));},
+         47: function(){ setTimeout(function() { $(searchElement).focus(); },250); },
+        103: function(){ if (g===1) {
+            g=0;
+            go(0);
+        } else { g=1;
+            setTimeout(function(){g=0;},1000);}
         }
-        return;
+    };
+
+$(window).load(function(){
+    $(window).keypress(function(event){
+        if (vim[event.keyCode]){
+            vim[event.keyCode]();
+        }
     });
 });
