5c4b0b0ae717ffa92ad72a281ffd82d1ff9a8b38
Joshua Mervine <jmervine@gmail.com>
2013-09-03 23:59:36 -0700

    Dropping number of unicorn workers.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,16 +2,14 @@ timeout 30
 stderr_path "log/unicorn_err.log"
 stdout_path "log/unicorn_out.log"
 pid         "log/unicorn.pid"
+worker_processes 2
 
 case ENV['RACK_ENV']
 when 'production'
   listen "/home/jmervine/mervine.net/sockets/unicorn.sock", :backlog => 64
-  worker_processes 4
 when 'staging'
-  worker_processes 4
   listen 9001
 else
-  worker_processes 2
   listen 8080
 end
 
